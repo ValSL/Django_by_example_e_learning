bd91f47c8c3c5452d6eb3b41f9084a6c14a9d75a
valid <valid.saley@gmail.com>
2020-08-13 17:36:33 +0300

    Displaying courses

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -5,13 +5,35 @@ from django.forms.models import modelform_factory
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.db.models import Count
 
 from .forms import ModuleFormSet
-from .models import Content, Course, Module
+from .models import Content, Course, Module, Subject
 
+
+# список всех курсов
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
+
+
+class CourseDetaiView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
 
 
 # обработка данных пришедшего json запроса для модели Module
