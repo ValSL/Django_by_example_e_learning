8f222ae4cf75ecca49b80e7239428d08a250a79e
valid <valid.saley@gmail.com>
2020-08-13 21:32:47 +0300

    Enrolling on courses & Accessing the course contents

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -13,6 +13,7 @@ from django.db.models import Count
 
 from .forms import ModuleFormSet
 from .models import Content, Course, Module, Subject
+from students.forms import CourseEnrollForm
 
 
 # список всех курсов
@@ -31,9 +32,14 @@ class CourseListView(TemplateResponseMixin, View):
                                         'courses': courses})
 
 
-class CourseDetaiView(DetailView):
+class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial={'course': self.object})
+        return context
 
 
 # обработка данных пришедшего json запроса для модели Module
