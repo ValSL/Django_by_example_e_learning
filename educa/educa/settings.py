17fccc2bb92a780c1a97164392d9930146e426c7
valid <valid.saley@gmail.com>
2020-08-14 12:26:13 +0300

    Rendering different types of content

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from django.urls import reverse_lazy
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -42,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'students.apps.StudentsConfig',
+
+    # вставка видео в шаблоны
+    'embed_video',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +133,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
