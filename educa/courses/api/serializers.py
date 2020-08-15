e5f9a12824462e4c9ca58d5b94511804519bd24c
valid <valid.saley@gmail.com>
2020-08-15 19:09:23 +0300

    Creating nested serializers

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,12 +1,29 @@
 from rest_framework import serializers
-from ..models import Subject
+from ..models import Subject, Course, Module
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     """
     Сериалайзер для модели Subject
-    Запаковка в JSON
+    Запаковка в питоновский словарь
     """
+
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    # ключ modules будет содержать инфу согласно ModuleSerializer вместо списка id'шников
+    modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview',
+                  'created', 'owner', 'modules']
