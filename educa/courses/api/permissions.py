81e22b4d001b4c82b28950dc7b9e053e7cb63ceb
valid <valid.saley@gmail.com>
2020-08-18 15:31:15 +0300

    Serializing course contents

--- educa/courses/api/permissions.py
+++ educa/courses/api/permissions.py
@@ -2,8 +2,5 @@ from rest_framework.permissions import BasePermission
 
 
 class IsEnrolled(BasePermission):
-    """
-    Ограничитель доступа для студентов, показ только курсов на которые юзер подписан
-    """
     def has_object_permission(self, request, view, obj):
         return obj.students.filter(id=request.user.id).exists()
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -7,7 +7,8 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from ..models import Subject, Course
-from .serializers import SubjectSerializer, CourseSerializer
+from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
+from .permissions import IsEnrolled
 
 
 class SubjectListView(generics.ListAPIView):
@@ -46,8 +47,16 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     @action(detail=True,  # detail - параметр говорит что работаем с одним объектом
             methods=['post'],
             authentication_classes=[BasicAuthentication],
-            permission_classes=(IsAuthenticated))
+            permission_classes=[IsAuthenticated])
     def enroll(self, request, *args, **kwargs):
-        course = self.get_object()
+        course = self.get_object()  # скорее всего берет pk из url'а
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True,
+            methods=['get'],
+            serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
