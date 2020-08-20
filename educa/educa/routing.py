b3a4d2d320db4f36f416df28985e7525306a8b3b
valid <valid.saley@gmail.com>
2020-08-20 20:16:41 +0300

    Routing

--- educa/educa/routing.py
+++ educa/educa/routing.py
@@ -1,5 +1,8 @@
-from channels.routing import ProtocolTypeRouter
+from chardet.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
 
+#  если не будет найдено соответствий то управление автоматически перейдет на стандартные вьюхи
 application = ProtocolTypeRouter({
-
+    'websocket': AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
 })
