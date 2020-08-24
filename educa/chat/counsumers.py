f34d4271e6b40a87edd174f70efcb6141e067964
ValSL <valid.saley@gmail.com>
2020-08-24 17:56:22 +0300

    Updating the consumer to broadcast messages

--- educa/chat/counsumers.py
+++ educa/chat/counsumers.py
@@ -1,5 +1,6 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -8,13 +9,32 @@ class ChatConsumer(WebsocketConsumer):
     """
 
     def connect(self):
+        # Every consumer has a scope with information about its connection, arguments passed by the URL, and the authenticated user, if any.
+        self.id = self.scope["url_route"]["kwargs"]["course_id"]
+        # Build the group name with the id of the course that the group corresponds to.
+        self.room_group_name = "chat_%s" % self.id
+        # join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
         # accept connection
         self.accept()
 
-    def disconnect(self):
-        pass
+    def disconnect(self, close_code):
+        # leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        self.send(text_data=json.dumps({'message': message}))
+        message = text_data_json["message"]
+        # send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "chat_message", "message": message,}
+        )
+
+    # receive message from room group
+    def chat_message(self, event):
+        # Send message to WebSocket
+        self.send(text_data=json.dumps(event))
