a9d338ed50253260c650e1367c7f199ccf1a6560
ValSL <valid.saley@gmail.com>
2020-08-24 18:24:17 +0300

    Adding context to the messages

--- educa/chat/counsumers.py
+++ educa/chat/counsumers.py
@@ -1,6 +1,7 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -9,6 +10,7 @@ class ChatConsumer(WebsocketConsumer):
     """
 
     def connect(self):
+        self.user = self.scope["user"]
         # Every consumer has a scope with information about its connection, arguments passed by the URL, and the authenticated user, if any.
         self.id = self.scope["url_route"]["kwargs"]["course_id"]
         # Build the group name with the id of the course that the group corresponds to.
@@ -29,9 +31,16 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        now = timezone.now()
         # send message to room group
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat_message", "message": message,}
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+                "user": self.user.username,
+                "datetime": now.isoformat(),
+            },
         )
 
     # receive message from room group
