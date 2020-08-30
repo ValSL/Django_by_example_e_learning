531b77351ef39f40ce95fc0e9627439600820a2d
ValSL <valid.saley@gmail.com>
2020-08-30 14:31:09 +0300

    Using Daphne for Django Channels

--- educa/educa/asgi.py
+++ educa/educa/asgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
+import django
+from channels.routing import get_default_application
 
-from django.core.asgi import get_asgi_application
+# from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
-
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educa.settings")
+django.setup()
+application = get_default_application()
